6a39ca95cc546e390aa5fb05e2f7f96600b587bd
qiancheng <qiancheng@jooz.vip>
2025-11-24 09:34:59 +0800

    更改history跳转为hash 跳转

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
